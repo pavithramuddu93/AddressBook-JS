a99095eabcf46146eaa2ee0df7db3678062aa6d3
pavithramuddu93 <pavithramuddu93>
2022-04-27 21:04:54 +0530

    UC2 added

--- AddressBook.js
+++ AddressBook.js
@@ -12,43 +12,65 @@ class AddressBook{
 
     get firstName(){return this._firstName}
     set firstName(firstName){
-        this._firstName = firstName;
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if(nameRegex.test(firstName))
+            this._firstName = firstName;
+        else throw "FirstName Is Incorrect Try Diff Name..!!"
     }
 
-    
     get lastName(){return this._lastName}
     set lastName(lastName){
-        this._lastName = lastName;
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if(nameRegex.test(lastName))
+            this._lastName = lastName;
+        else throw "LastName Is Incorrect Try Diff Name..!!"
     }
 
-    
     get address(){return this._address}
     set address(address){
-        this._address = address;
+        let nameRegex = RegExp('[A-Z,a-z,0-9]{3,}$');
+        if(nameRegex.test(address))
+            this._address = address;
+        else throw "Address In Incorrect Try Diff..!!"
     }
 
-    
     get city(){return this._city}
     set city(city){
-        this._city = city;
+        let nameRegex = RegExp('[A-Z,a-z,0-9]{3,}$');
+        if(nameRegex.test(city))
+            this._city = city;
+        else throw "Incorrect City Name Try Diff..!!"
     }
-
     
+    get state(){return this._state}
+    set state(state){
+        let nameRegex = RegExp('[A-Z,a-z,0-9]{3,}$');
+        if(nameRegex.test(state))
+            this._state = state;
+        else throw "Incorrect State Name Try Diff..!!"
+    }
     get zip(){return this._zip}
     set zip(zip){
-        this._zip = zip;
+        let nameRegex = RegExp('^[1-9]{1}[0-9]{5}')
+        if(nameRegex.test(zip))
+            this._zip = zip;
+        else throw "Incorrect zip Pattern Try Diff..!"
     }
-
-    
+ 
     get phoneNum(){return this._phoneNum}
     set phoneNum(phoneNum){
-        this._phoneNum = phoneNum;
+        let nameRegex = RegExp('^[0-9]{2}[: :][0-9]{10}')
+        if(nameRegex.test(phoneNum))
+            this._phoneNum = phoneNum;
+        else throw "Incorrect Phone Num..!!"
     }
 
-    
     get email(){return this._email}
     set email(email){
-        this._email = email;
+        let nameRegex = RegExp('^abc[a-zA-Z0-9.+-]*@[a-z0-9]*[.][a-z]{2,5}[.,a-z]*$')
+        if(nameRegex.test(email))
+            this._email = email;
+        else throw "Incorrect Email Address..!"
     }
 
     toString(){
@@ -56,5 +78,9 @@ class AddressBook{
     }
 }
 
-let addressBook = new AddressBook("Deep","Maywade","Betul","Badora","MP","450001","32434523455","deep@gmail.com")
-console.log(addressBook.toString());
+try {
+    let addressBook = new AddressBook("Deep","Maywade","Betul","Badora","Mdhya","450001","91 8392949492","abcdeep@gmail.com")
+    console.log(addressBook.toString());
+} catch (e) {
+    console.error(e)
+}
